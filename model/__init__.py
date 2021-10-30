9a2cda5a6055f7e45854faec79cbf480ca14a4d9
Suyeol Yun <syyun@mit.edu>
2021-10-30 14:42:13 -0400

    lint on model

--- model/__init__.py
+++ model/__init__.py
@@ -4,20 +4,21 @@ import torchvision.models as models
 
 from model.resnet import resnet18, resnet34
 
+
 def get_model(args, device=None):
 
-    if args.model == 'cnn':
+    if args.model == "cnn":
         model = resnet18()
         pass
 
-    elif args.model == 'cnn_prev':
+    elif args.model == "cnn_prev":
         model = models.resnet18(pretrained=False).to(device)
-        # model.conv1 = nn.Conv2d(1, 64, (7, 7), (2, 2), (3, 3), bias=False)
-        # model.avgpool = nn.AdaptiveAvgPool2d(1)
-        # model.fc = nn.Linear(in_features=512, out_features=1, bias=True)
+        model.conv1 = nn.Conv2d(1, 64, (7, 7), (2, 2), (3, 3), bias=False)
+        model.avgpool = nn.AdaptiveAvgPool2d(1)
+        model.fc = nn.Linear(in_features=512, out_features=1, bias=True)
 
         # if args.regression:
-            # model.
+        # model.
 
     else:
         model_module = importlib.import_module("model." + args.model)
