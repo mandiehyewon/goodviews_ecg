3f2adcc06442243c8f9079511f2062be04954fcd
Suyeol Yun <syyun@mit.edu>
2021-10-30 14:27:10 -0400

    comment out redundant classifier block

--- model/__init__.py
+++ model/__init__.py
@@ -8,12 +8,13 @@ def get_model(args, device=None):
 
     if args.model == 'cnn':
         model = resnet18()
+        pass
 
     elif args.model == 'cnn_prev':
-        model = models.resnet18(pretrained=True).to(device)
-        model.conv1 = nn.Conv2d(1, 64, (7, 7), (2, 2), (3, 3), bias=False)
-        model.avgpool = nn.AdaptiveAvgPool2d(1)
-        model.fc = nn.Linear(in_features=512, out_features=1, bias=True)
+        model = models.resnet18(pretrained=False).to(device)
+        # model.conv1 = nn.Conv2d(1, 64, (7, 7), (2, 2), (3, 3), bias=False)
+        # model.avgpool = nn.AdaptiveAvgPool2d(1)
+        # model.fc = nn.Linear(in_features=512, out_features=1, bias=True)
 
         # if args.regression:
             # model.
@@ -25,5 +26,5 @@ def get_model(args, device=None):
 
     model = model.to(device)
     model = nn.DataParallel(model)
-    
+
     return model
