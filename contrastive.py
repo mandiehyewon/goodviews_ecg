4dac46663ccfc62c0b9ae67ead7329e07410d309
Suyeol Yun <syyun@mit.edu>
2021-11-08 18:26:24 -0500

    fix nan loss

--- contrastive.py
+++ contrastive.py
@@ -1,29 +1,33 @@
 import torch
 import torch.nn.functional as F
 import numpy as np
+from config import args
 
 
 def get_contrastive_loss(features, labels, args):
-    # temperature = 0.07
-    # batch_size = 64
     features = F.normalize(
         features, p=2, dim=1
     )  # normalize for each row's L-2 norm become 1; this makes torch.matmul produce cosine similiarity matrix without scaling factors
     similarity_matrix = torch.matmul(
         features, features.T
     )  # similiarty matrix.shape = [batch_size, batch_size]
-    # labels = torch.randint(2, similarity_matrix.shape)
 
     similarity_matrix = similarity_matrix * (
         1 - torch.eye(args.batch_size, args.batch_size)
     )  # make each diagonal to be zero
     similarity_matrix = similarity_matrix / args.temperature
     similarity_matrix_exp = torch.exp(similarity_matrix)
+    # print(labels)
+    # neg_labels = (labels == 0).type(torch.uint8)
     numerators = torch.sum(torch.mul(similarity_matrix_exp, labels), dim=1)
+    # denominators = torch.sum(torch.mul(similarity_matrix_exp, neg_labels), dim=1)
     denominators = torch.sum(similarity_matrix_exp, dim=1)
+    p = torch.div(numerators, denominators)
+    # print("p", p, 0 < p, p <1)
     eps = 1e-7
-    loss = -torch.log(torch.div(numerators, denominators + eps))
+    loss = - torch.log(p+eps)
     loss = loss.sum(dim=0)
+    loss /= args.batch_size
     return loss
 
 
@@ -31,22 +35,6 @@ if __name__ == "__main__":
     temperature = 0.07  #
     batch_size = 64
     dim = 100
+    labels = torch.randint(2, [args.batch_size, args.batch_size])
     features = torch.rand(batch_size, dim)
-    features = F.normalize(
-        features, p=2, dim=1
-    )  # normalize for each row's L-2 norm become 1; this makes torch.matmul produce cosine similiarity matrix without scaling factors
-    similarity_matrix = torch.matmul(
-        features, features.T
-    )  # similiarty matrix.shape = [batch_size, batch_size]
-    labels = torch.randint(2, similarity_matrix.shape)
-
-    similarity_matrix = similarity_matrix * (
-        1 - torch.eye(batch_size, batch_size)
-    )  # make each diagonal to be zero
-    similarity_matrix = similarity_matrix / temperature
-    similarity_matrix_exp = torch.exp(similarity_matrix)
-    numerators = torch.sum(torch.mul(similarity_matrix_exp, labels), dim=1)
-    denominators = torch.sum(similarity_matrix_exp, dim=1)
-    loss = -torch.log(torch.div(numerators, denominators))
-    loss = loss.sum(dim=0)
-    loss.backward()
+    get_contrastive_loss(features, labels, args)
